c944fcfcfbda65be992cc7571ed2361c68005097
PedroGranDiosEmperador <99508015+PedroGranDiosEmperador@users.noreply.github.com>
2025-05-24 22:11:51 -0600

    Inicio del BackEnd
    Aaaaaaaaaaaaaaaaaaaaah

---
+++ BackEndCVMatchMaking/src/main/java/com/example/BackEndCVMatchMaking/BackEndCvMatchMakingApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackEndCVMatchMaking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndCvMatchMakingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndCvMatchMakingApplication.class, args);
+	}
+
+}
---
+++ BackEndCVMatchMaking/src/test/java/com/example/BackEndCVMatchMaking/BackEndCvMatchMakingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackEndCVMatchMaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndCvMatchMakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
