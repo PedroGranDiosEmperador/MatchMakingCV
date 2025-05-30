db713fe19edf4042d8c479cb276895dba1c6fec3
PedroGranDiosEmperador <99508015+PedroGranDiosEmperador@users.noreply.github.com>
2025-05-30 00:51:37 -0600

    Finalizacion metodo de MatchMaking
    Mas o menos, terminamos con el metodo para darle peso a los trabajos y crear una lista basado en ello, ahora tenemos que ordenar esa lista wups wupsie dooh

--- BackEndCVMatchMaking/src/main/java/com/example/BackEndCVMatchMaking/services/MatchMakingService.java
+++ BackEndCVMatchMaking/src/main/java/com/example/BackEndCVMatchMaking/services/MatchMakingService.java
@@ -1,5 +1,6 @@
 package com.example.BackEndCVMatchMaking.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,37 +16,8 @@ public class MatchMakingService {
     private CurriculumVitaeService curriculumService;
     
     // < -- PESOS PARA LA EVALUACION DE APLICANTES [TRABAJO --> CURRICULUM]
-    private float pagoEsperadoT = 0.20f;
-    private float pesoHBlandasT = 0.30f;
-    private float pesoHFuertesT = 0.50f;
-
-    
-
-
-    /* METODOS PARA EL CALCULO DE COINCIDENCIA */
-    // public float SumaHabilidades(CurriculumVitae cv, Trabajo t){
-
-    //     List<String> listaHabilidadesBlandas= t.getHabilidadesBlandas();
-    //     List<String> listaHabilidadesFuertes= t.getHabilidadesFuertes();
-    //     List<String> habilidadesFuertesCliente= cv.getHabilidadesFuertes();
-    //     List<String> habilidadesBlandasCliente= cv.getHabilidadesBlandas();
-    //     //Wah, hay que hacerlo super facil, que tal si hacemos nuevos objetos llamados resultados que 
-    //     //son la union de un curricullum vitalae y de los puntos que obtiene aca
-
-    //     int puntosHabilidadesFuertes=0;
-    //     int puntosHabilidadesDebiles=0;
-    //     float pesoHabilidadesFuertes=1;
-    //     float pesoHabilidadesDebiles=0.5F;
-    //     //FOR para determinar cuantos puntos fuertes coinciden
-    //     for (String habilidadCliente : habilidadesBlandasCliente) {
-    //         for (String string2 : listaHabilidadesBlandas) {
-                
-    //         }
-    //     }
-    // 
-    // < -- PESOS PARA LA EVALUACION DE APLICANTES [TRABAJO --> CURRICULUM]
-    private float pagoEsperadoT = 0.20f;
-    private float pesoHBlandasT = 0.30f;
+    private float pagoEsperadoT = 0.30f;
+    private float pesoHBlandasT = 0.20f;
     private float pesoHFuertesT = 0.50f;
 
     //Cuando el pago es muy parecido a lo que el cliente busca
@@ -54,6 +26,11 @@ public class MatchMakingService {
     private float pesoPagoSuperior= 1.5f;
     //cuando el pago es muy inferior a lo que el cliente busca;
     private float pesoPagoInferior=0f;
+
+    //Tipos de trabajos
+    private float trabajo3=1f;
+    private float trabajo2=0.80f;
+    private float trabajo1= 0.5f;
 
     
 
@@ -94,11 +71,10 @@ public class MatchMakingService {
         //Todas las variables necesarias...
         List <Trabajo> listaTrabajos= trabajoService.getAllTrabajo();
         CurriculumVitae cv= curriculumService.getcurriculumVitaeById(id);
-        List <Resultado> listaResultante;
-        List <Trabajo> listaAuxiliarTrabajo;
+        List <Resultado> listaResultante= new ArrayList<>();
 
-       
         //Metodo If checando que el Curriculum exista
+
         if(cv!=null){
             for (Trabajo trabajo : listaTrabajos) {
                if(trabajo.getTipoTrabajo()!= cv.getTipoTrabajo())
@@ -113,16 +89,46 @@ public class MatchMakingService {
                 float resultadoSueldo;
                 float resultadoHabilidadesFuertes;
                 float resultadoHabilidadesDebiles;
+                int tipoTrabajo;
         //Variables que vamos a trabajar
                 resultadoSueldo=evaluacionSueldo(cv.getSueldoEsperado(), trabajo.getSueldo());
-                resultadoHabilidadesDebiles= SumaHabilidades(cv.getHabilidadesBlandas().toArray(),trabajo.getHabilidadesBlandas());
+                resultadoHabilidadesDebiles= SumaHabilidades(cv.getHabilidadesBlandas(),trabajo.getHabilidadesBlandas());
+                resultadoHabilidadesFuertes= SumaHabilidades(cv.getHabilidadesFuertes(), trabajo.getHabilidadesFuertes());
+                
+                resultadoHabilidadesDebiles= resultadoHabilidadesDebiles*pesoHBlandasT;
+                resultadoHabilidadesFuertes= resultadoHabilidadesFuertes*pesoHFuertesT;
 
+                float resultadoHabilidades= resultadoHabilidadesDebiles+resultadoHabilidadesFuertes;
 
-            }
+                resultadoSueldo= evaluacionSueldo(cv.getSueldoEsperado(), trabajo.getSueldo());
+                resultadoSueldo= resultadoSueldo*pagoEsperadoT;
+                float pesoTrabajo= resultadoHabilidades+resultadoSueldo;
+               
+                tipoTrabajo= tipificarTrabajo(pesoTrabajo);
+                Resultado resultado= new Resultado(trabajo, pesoTrabajo, tipoTrabajo);
+
+                listaResultante.add(resultado);
+                
+           }
+           //Finalizacion de ciclo for, se manda la listaResultante
+           return listaResultante;
         }
         else{
             return null;
         }
     } 
+    public int tipificarTrabajo(float pt){
+        if(pt >=trabajo3){
+            return 3;
+        }
+        if(pt >=trabajo2){
+            return 2;
+        }
+        if(pt >= trabajo1){
+            return 1;
+        }
+        return 0;
+    }
+
 }
 
